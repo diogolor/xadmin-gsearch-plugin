710789c3473722b6045d0a0327dc74dc5866eda1
diogo <diogo@fabricadigigital.com.br>
2026-03-18 15:04:41 -0300

    v1.3.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
 	name='xadmin-gsearch-plugin',
-	version='1.2.0',
+	version='1.3.0',
 	include_package_data=True,
 	packages=['xplugin_gsearch', 'xplugin_gsearch.templatetags', 'xplugin_gsearch.views'],
 	url='https://github.com/alexsilva/xadmin-gsearch-plugin',
